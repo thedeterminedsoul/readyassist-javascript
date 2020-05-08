a9db13002efc86ded6bc580b7b7078dc2c9d22a0
Pawan Tejwani <>
2020-05-08 10:29:43 +0530

    adding functions file

---
+++ src/03_functions.js
@@ -0,0 +1,18 @@
+// block of code
+
+let trainees = ['prashant', 'ashmi', 'praveen', 'raghul', 'Abhilash', 'Arun']
+
+function printHelloWorld() {
+    console.log("Hello world")
+}
+
+function printNames() {
+    console.log("====================")
+    for(i =0; i < trainees.length; i++) {
+        console.log(trainees[i])
+    }
+    console.log("====================")
+}
+
+printNames()
+printNames()
