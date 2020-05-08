0b206ef304085f7ab384d79c88dd34b1db05bd16
Pawan Tejwani <>
2020-05-08 10:10:39 +0530

    adding arrays file

---
+++ src/00_arrays.js
@@ -0,0 +1,11 @@
+
+
+let trainees = ['prashant', 'ashmi', 'praveen', 'raghul', 'Abhilash', 'Arun']
+
+console.log(trainees[0])
+console.log(trainees[1])
+console.log(trainees[2])
+console.log(trainees[3])
+console.log(trainees[4])
+console.log(trainees[5])
+
