6496baa1c443e74a8764ea6b9de48c693b924e0f
Pawan Tejwani <>
2020-05-08 10:43:18 +0530

    adding calculator

---
+++ src/04_calculator.js
@@ -0,0 +1,30 @@
+
+function add(n1, n2) {
+    let result = n1 + n2
+    return result
+}
+
+function sub(n1, n2) {
+    let result = n1 - n2;
+    return result;
+}
+
+function mul(n1, n2) {
+    let result = n1 * n2;
+    return result;
+}
+
+function div(n1, n2) {
+    let result = n1 / n2;
+    return result;
+}
+
+let addition_result = add(5, 5)
+let subtraction_result = sub(5, 5)
+let multiplication_result = mul(5, 5)
+let division_result = div(5, 5)
+
+console.log(`Addtion: ${addition_result}`)
+console.log(`Subtraction: ${subtraction_result}`)
+console.log(`Multiplication: ${multiplication_result}`)
+console.log(`Division: ${division_result}`)
